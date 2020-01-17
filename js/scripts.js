3ba25ab4acd84a8d5a6d7a05ed1781b414b7dc2c
Keturah Howard <keturah.dev@gmail.com>
2020-01-17 10:27:42 -0800

    add branching to the scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,31 @@ $(document).ready(function () {
     var question5 = parseInt($('#question5').val());
 
     score = question1 + question2 + question3 + question4 + question5;
+    // console.log('score:', score);
 
+    if (score < 60) {
+      $('#javascript').show(); //
+      $('#c').hide();
+      $('#python').hide();
+      $('#ruby').hide();
 
-  })
-})
+    } else if (score >= 60 && score <= 175) {
+      $('#javascript').hide();
+      $('#c').hide();
+      $('#python').show(); //
+      $('#ruby').hide();
+    } else if (score > 175 && score <= 299) {
+      $('#javascript').hide();
+      $('#c').show();
+      $('#python').hide();
+      $('#ruby').hide();
+
+    } else if (score >= 300) {
+      $('#javascript').hide();
+      $('#c').hide();
+      $('#python').hide();
+      $('#ruby').show();
+    }
+  });
+  // console.log('running')
+});
