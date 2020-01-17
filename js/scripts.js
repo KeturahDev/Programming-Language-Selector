13542ff3ee351ace599b02065470293631a3258d
Keturah Howard <keturah.dev@gmail.com>
2020-01-17 15:01:56 -0800

    add new functions and branching into scripts file, placing score into a string to compare placeholders rather than actual mathmatical value of numbers given by user-input

--- js/scripts.js
+++ js/scripts.js
@@ -8,29 +8,88 @@ $(document).ready(function () {
     var question4 = parseInt($('#question4').val());
     var question5 = parseInt($('#question5').val());
 
-    score = question1 + question2 + question3 + question4 + question5;
+    var score = question1 + question2 + question3 + question4 + question5;
+
+    var placeHolder = 90000;
+    score += placeHolder;
     console.log('score:', score);
 
+    var scoreString = String(score);
+
+    console.log('placeHolder:', scoreString[0]);
+    var ruby = scoreString[1];
+    var c = scoreString[2];
+    var python = scoreString[3];
+    var javascript = scoreString[4];
+
+    $('#javascript').hide();
+    $('#c').hide();
+    $('#python').hide();
+    $('#ruby').hide();
+
+    function showRuby() {
+      $('#ruby').show();
+    };
+    function showC() {
+      $('#c').show();
+    };
+    function showPython() {
+      $('#python').show();
+    };
+    function showJavaScript() {
+      $('#javascript').show();
+    };
+
+    if (javascript > ruby && javascript > c && javascript > python) {
+      showJavaScript()
+    } else if (python > ruby && python > c && python > javascript) {
+      showPython()
+    } else if (c > ruby && c > python && c > javascript) {
+      showC()
+    } else if (ruby > c && ruby > python && c > javascript) {
+      showRuby()
+    } else {
+      if (javascript === ruby) {
+        showJavaScript();
+        showRuby();
+      } else if (javascript === c) {
+        showJavaScript();
+        showC();
+      } else if (javascript === python) {
+        showJavaScript();
+        showPython();
+      } else if (c === python) {
+        showC();
+        showPython();
+      } else if (c === ruby) {
+        showC();
+        showRub();
+      } else if (python === ruby) {
+        showPython();
+        showRuby();
+      }
+    }
+
     if (score < 120) {
-      $('#javascript').show();                //
+      $('#javascript').show();
       $('#c').hide();
       $('#python').hide();
       $('#ruby').hide();
     } else if (score >= 120 && score <= 300) {
       $('#javascript').hide();
       $('#c').hide();
-      $('#python').show();                    //
+      $('#python').show();
       $('#ruby').hide();
     } else if (score > 300 && score <= 400) {
       $('#javascript').hide();
-      $('#c').show();                         //
+      $('#c').show();
       $('#python').hide();
       $('#ruby').hide();
     } else if (score >= 400) {
       $('#javascript').hide();
       $('#c').hide();
       $('#python').hide();
-      $('#ruby').show();                      //
+      $('#ruby').show();
     }
     // if (score < 60) {
     //   $('#javascript').show();                //
