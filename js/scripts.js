ab6786a23ddf6146e7673682e457acd69695f311
Keturah Howard <keturah.dev@gmail.com>
2020-01-17 11:30:54 -0800

    attempt a different ratio for submitting accurate rules based on the user’s input score, and add content to the result divs in the html

--- js/scripts.js
+++ js/scripts.js
@@ -9,29 +9,50 @@ $(document).ready(function () {
     var question5 = parseInt($('#question5').val());
 
     score = question1 + question2 + question3 + question4 + question5;
-    // console.log('score:', score);
+    console.log('score:', score);
 
-    if (score < 60) {
-      $('#javascript').show(); //
+    if (score < 120) {
+      $('#javascript').show();                //
       $('#c').hide();
       $('#python').hide();
       $('#ruby').hide();
-    } else if (score >= 60 && score <= 175) {
+    } else if (score >= 120 && score <= 300) {
       $('#javascript').hide();
       $('#c').hide();
-      $('#python').show(); //
+      $('#python').show();                    //
       $('#ruby').hide();
-    } else if (score > 175 && score <= 299) {
+    } else if (score > 300 && score <= 400) {
       $('#javascript').hide();
-      $('#c').show();
+      $('#c').show();                         //
       $('#python').hide();
       $('#ruby').hide();
-    } else if (score >= 300) {
+    } else if (score >= 400) {
       $('#javascript').hide();
       $('#c').hide();
       $('#python').hide();
-      $('#ruby').show();
+      $('#ruby').show();                      //
     }
+    // if (score < 60) {
+    //   $('#javascript').show();                //
+    //   $('#c').hide();
+    //   $('#python').hide();
+    //   $('#ruby').hide();
+    // } else if (score >= 60 && score <= 175) {
+    //   $('#javascript').hide();
+    //   $('#c').hide();
+    //   $('#python').show();                    //
+    //   $('#ruby').hide();
+    // } else if (score > 175 && score <= 349) {
+    //   $('#javascript').hide();
+    //   $('#c').show();                         //
+    //   $('#python').hide();
+    //   $('#ruby').hide();
+    // } else if (score >= 350) {
+    //   $('#javascript').hide();
+    //   $('#c').hide();
+    //   $('#python').hide();
+    //   $('#ruby').show();                      //
+    // }
   });
   // console.log('running')
 });
